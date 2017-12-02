36661aa63470e9d88e13192ee9c91c0c37dc372e
raymondsze <szer0601@gmail.com>
2017-12-02 20:01:15 +0800

    rough try on emerging_usd

--- test.py
+++ test.py
@@ -6,26 +6,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, Dropout, LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
 filename = 'godiva.json'
 json_data = json.load(open(filename))
 
-data = numpy.array(list(map(lambda sold_data: int(sold_data['sold_count_Icecream_emerging'].replace(",","")), json_data['result']['records'])))
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+# int(data['emerging_usd'].replace(",", ""))
+data = numpy.array(list(map(lambda data: [int(data['emerging_usd'].replace(",", "")), months.index(data['month'])], json_data['result']['records'])))
 
 # # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
+		a = dataset[i:(i+look_back), :]
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return numpy.array(dataX), numpy.array(dataY)
 # fix random seed for reproducibility
-# numpy.random.seed(7)
+numpy.random.seed(7)
 # load the dataset
 dataframe = pd.DataFrame(data)
 
@@ -35,24 +36,27 @@ dataset = dataset.astype('float32')
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 # split into train and test sets
-train_size = int(len(dataset) * 0.67)
+train_size = 12 # int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 # reshape into X=t and Y=t+1
-look_back = 3
+look_back = 1
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
+
 # reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 2))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 2))
+
 # create and fit the LSTM network
 batch_size = 1
 model = Sequential()
-model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
-model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 2), stateful=True, return_sequences=True))
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 2)))
+# model.add(Dropout(0.5))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-for i in range(100):
+for i in range(500):
 	model.fit(trainX, trainY, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
 	model.reset_states()
 # make predictions
@@ -60,14 +64,15 @@ trainPredict = model.predict(trainX, batch_size=batch_size)
 model.reset_states()
 testPredict = model.predict(testX, batch_size=batch_size)
 # invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
+trainPredict = scaler.inverse_transform(list(map(lambda x:[x, 0], trainPredict)))
+trainY = scaler.inverse_transform(list(map(lambda x:[x, 0], trainY)))
+testPredict = scaler.inverse_transform(list(map(lambda x:[x, 0], testPredict)))
+testY = scaler.inverse_transform(list(map(lambda x:[x, 0], testY)))
+
 # calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+trainScore = math.sqrt(mean_squared_error(list(map(lambda x:x[0], trainY)), trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(list(map(lambda x:x[0], testY)), testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
@@ -78,7 +83,7 @@ testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
 testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+plt.plot(list(map(lambda x:x[0], scaler.inverse_transform(dataset))))
+plt.plot(list(map(lambda x:x[0], trainPredictPlot)))
+plt.plot(list(map(lambda x:x[0], testPredictPlot)))
 plt.show()
